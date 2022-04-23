c806075184dd5cd3ac3908485cb9f228d5ea65fc
William Swartzendruber <wswartzendruber@gmail.com>
2022-04-23 07:28:01 -0600

    Changing Decoder API.

--- unikode/src/commonMain/kotlin/Decoder.kt
+++ unikode/src/commonMain/kotlin/Decoder.kt
@@ -19,11 +19,11 @@ package org.unikode
 public abstract class Decoder {
 
     public abstract fun decode(
-        destination: CharArray,
-        destinationOffset: Int = 0,
         source: ByteArray,
+        destination: CharArray,
         sourceStartIndex: Int = 0,
         sourceEndIndex: Int = source.size,
+        destinationOffset: Int = 0,
     ): Int
 
     public abstract fun maxCharsNeeded(byteCount: Int): Int
