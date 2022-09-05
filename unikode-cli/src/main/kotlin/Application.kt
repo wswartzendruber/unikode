a612812ccf89febbdaa8bec780ae92a2747a0751
William Swartzendruber <wswartzendruber@gmail.com>
2022-09-04 18:50:04 -0600

    Updating unikode-cli to support all encodings.

--- unikode-cli/src/main/kotlin/Application.kt
+++ unikode-cli/src/main/kotlin/Application.kt
@@ -4,10 +4,21 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 import kotlin.system.exitProcess
 
-import org.unikode.Utf8Decoder
+import org.unikode.BOM_CHAR
 import org.unikode.Utf8Encoder
-import org.unikode.stf7.Stf7Decoder
+import org.unikode.Utf8Decoder
+import org.unikode.Utf16LeEncoder
+import org.unikode.Utf16LeDecoder
+import org.unikode.Utf16BeEncoder
+import org.unikode.Utf16BeDecoder
+import org.unikode.Utf32LeEncoder
+import org.unikode.Utf32LeDecoder
+import org.unikode.Utf32BeEncoder
+import org.unikode.Utf32BeDecoder
+import org.unikode.bad.Cesu8Encoder
+import org.unikode.bad.Cesu8Decoder
 import org.unikode.stf7.Stf7Encoder
+import org.unikode.stf7.Stf7Decoder
 
 fun main(args: Array<String>) {
 
@@ -17,27 +28,47 @@ fun main(args: Array<String>) {
         )
     }
 
+    var passedBom = false
     val inputStream = FileInputStream(args[1])
     val outputStream = FileOutputStream(args[3])
     val encoderCallback = { byte: Byte ->
         outputStream.write(byte.toInt())
     }
-    val encoder =
+    val (encoder, useBom) =
         when (args[2]) {
-            "utf8" -> Utf8Encoder(encoderCallback)
-            "stf7" -> Stf7Encoder(encoderCallback)
+            "utf8" -> Pair(Utf8Encoder(encoderCallback), false)
+            "utf8-bom" -> Pair(Utf8Encoder(encoderCallback), true)
+            "utf16le-bom" -> Pair(Utf16LeEncoder(encoderCallback), true)
+            "utf16be-bom" -> Pair(Utf16BeEncoder(encoderCallback), true)
+            "utf32le-bom" -> Pair(Utf32LeEncoder(encoderCallback), true)
+            "utf32be-bom" -> Pair(Utf32BeEncoder(encoderCallback), true)
+            "cesu8" -> Pair(Cesu8Encoder(encoderCallback), false)
+            "stf7" -> Pair(Stf7Encoder(encoderCallback), false)
             else -> throw IllegalArgumentException("Invalid output encoding specified.")
         }
     val decoderCallback = { char: Char ->
-        encoder.input(char)
+        if (passedBom) {
+            encoder.input(char)
+        } else {
+            if (char != BOM_CHAR)
+                encoder.input(char)
+            passedBom = true
+        }
     }
     val decoder =
         when (args[0]) {
             "utf8" -> Utf8Decoder(decoderCallback)
+            "utf16le" -> Utf16LeDecoder(decoderCallback)
+            "utf16be" -> Utf16BeDecoder(decoderCallback)
+            "utf32le" -> Utf32LeDecoder(decoderCallback)
+            "utf32be" -> Utf32BeDecoder(decoderCallback)
+            "cesu8" -> Cesu8Decoder(decoderCallback)
             "stf7" -> Stf7Decoder(decoderCallback)
             else -> throw IllegalArgumentException("Invalid input encoding specififed.")
         }
 
+    if (useBom)
+        encoder.input(BOM_CHAR)
     for (byte in inputStream.readBytes())
         decoder.input(byte)
 
